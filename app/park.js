813a448017b8bc29685abdf52bbb8b92b67aeeda
kimnorth <benignhumour@gmail.com>
2017-04-04 18:04:51 +0100

    Re-wrote earlier code to make the park class create an array of animals and getting the length property of that array. Tested my addDinosaur method.

--- app/park.js
+++ app/park.js
@@ -1,9 +1,14 @@
 var Park = function(){
-  this.enclosureSize = 0;
+  this.enclosureSize = [];
 
   this.getEnclosureSize = function(){
-    return this.enclosureSize;
+    return this.enclosureSize.length;
   }
+
+  this.addDinosaur = function(dinosaur){
+    this.enclosureSize.push(dinosaur);  
+  }
+  
 }
 
 module.exports = Park;
--- test/park.spec.js
+++ test/park.spec.js
@@ -1,16 +1,24 @@
 var assert = require('assert');
 var Park = require('../app/park.js');
+var Dinosaur = require('../app/dinosaur.js');
 
 describe('Park', function(){
 
   var park;
+  var velociraptor;
 
   beforeEach(function(){
     park = new Park();
+    velociraptor = new Dinosaur();
   });
 
   it('Enclosure should start empty', function(){
     assert.strictEqual(0, park.getEnclosureSize());
+  })
+
+  it('Can add dinosaurs to the enclosure', function(){
+    park.addDinosaur(velociraptor);
+    assert.strictEqual(1, park.getEnclosureSize());
   })
 
 });
